933b53f4bdd8bd184624e1f5733a6a791bf841d5
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2014-05-29 23:59:02 -0700

    Crude BellmanBot in place

--- Canopy2048/BellmanBot.fs
+++ Canopy2048/BellmanBot.fs
@@ -6,34 +6,31 @@ module BellmanBot =
 
     let moves = [| Up; Down; Left; Right; |]
 
-    let nextStates (state:State) (move:Move) =
-        let _,next = execute state move
-        let emptyCells = empty next
-        seq { for key in emptyCells do
-                yield (next |> Map.add key 2)
-                yield (next |> Map.add key 4) }
+    let p2, p4 = 0.9, 0.1
 
-    let valueOf (state:State) (move:Move) =
-        
-        let score,updated = execute state move
-        
-        let empties = empty updated
-        match (Seq.isEmpty empties) with
-        | true  -> score
-        | false ->
-            seq {
-                for choice in moves do
-                    let results = seq {
-                        for key in empties do 
-                            let ``2 added`` = updated |> Map.add key 2 
-                            for choice in moves do
-                                yield (execute ``2 added`` choice |> fst)
-                            let ``4 added`` = updated |> Map.add key 4
-                            for choice in moves do
-                                yield (execute ``4 added`` choice |> fst)
-                        } 
-                    yield results |> Seq.min
-            } |> Seq.max |> fun x -> x + score
+    let nextStates (state:State) =
+        let openSpaces = empty state
+        let count = openSpaces |> Seq.length |> float
+        [ for pos in openSpaces do
+            yield p2/count, state |> Map.add pos 2
+            yield p4/count, state |> Map.add pos 4 ]
+                
+    let rec scoreOf (state:State) (move:Move) (depth:int) =
+        let score,next = execute state move
+        if depth = 0 
+        then (float score)
+        else 
+            let nexts = nextStates next
+            let d = depth - 1
+            let extra = 
+                nexts 
+                |> Seq.sumBy (fun (p,s) -> p * (scoreOf s (bestMove s d) d))
+            float score + extra
+
+    and bestMove (state:State) (depth:int) =
+        moves 
+        |> Seq.maxBy (fun move -> 
+            scoreOf state move depth)
     
     let rng = System.Random()
     let shuffle (arr:'a []) =
@@ -45,5 +42,5 @@ module BellmanBot =
             arr.[j] <- temp
         arr
 
-    let decide (state:State) =
-        moves |> shuffle |> Array.maxBy (valueOf state)        
+    let decide (state:State) = bestMove state 2
+        //moves |> shuffle |> Array.maxBy (bestMove state 2)        
